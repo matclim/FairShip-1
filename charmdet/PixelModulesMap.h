f67c23f6ec8001750579709469e54b30c489a931
Matei Climescu <matei.climescu@cern.ch>
2019-02-19 15:30:29 +0100

    Added mapping class

---
+++ charmdet/PixelModulesMap.h
@@ -0,0 +1,17 @@
+#ifndef PIXELMODULESMAP.H
+#define PIXELMODULESMAP.H
+
+#include "FairModule.h"                 // for FairModule
+#include "FairDetector.h"                  // for FairDetector
+#include "Math.h"
+class PixelModulesMap: public PixelModules, public PixelModulesPoint
+{
+	public:
+	
+	private:
+	
+	
+	
+	
+	
+	}
