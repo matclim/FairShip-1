b96b705234265b1553fcd640187f1408fec80ac7
Matei Climescu <matei.climescu@cern.ch>
2019-03-19 21:12:30 +0100

    filled the arrays

--- charmdet/ShipPixelHit.cxx
+++ charmdet/ShipPixelHit.cxx
@@ -3,6 +3,15 @@
 
 ShipPixelHit::ShipPixelHit(Int_t detID, Float_t digi) : ShipHit(detID, digi) {}
 
+ShipPixelHit::Setpixelx(0, 0.0225)
+ShipPixelHit::Setpixelx(167, 0.0225)
+ShipPixelHit::Setpixelx(168, 0.0225)
+ShipPixelHit::Setpixelx(335, 0.0225)
+
+for(int i=1;i<=335;i++){
+if(i!=167 || i!=168){
+ShipPixelHit::Setpixelx(i,0.0125);}
+}
 
 
 
