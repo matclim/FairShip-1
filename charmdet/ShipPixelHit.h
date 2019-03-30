6027921c57948ed986f77687171cb863d86108df
s6maclim <s6maclim@wn024.baf.physik.uni-bonn.de>
2019-03-30 15:32:41 +0100

    started digi

--- charmdet/ShipPixelHit.h
+++ charmdet/ShipPixelHit.h
@@ -28,6 +28,22 @@ public:
    void Print();
    int32_t GetTimeOverThreshold() const {return fdigi*25 ; }
 
+   float GetX();
+   float GetY();
+   float GetZ();
+   int GetRow();
+   int GetCol();
+   int GetMod();
+   void SetXYZ(float x, float y, float z);
+   void SetDetectorID(long detID);
+   void SetMCR(unsigned mod, unsigned col, unsigned row);
+   static std::array<float, ncol> pixelX;
+   static std::array<float, nrow> pixelY;
+   static std::array<float, nmod> pixelZ;
+   void Convert();
+
+	
+
 private:
    /** Copy constructor **/
    ShipPixelHit(const ShipPixelHit &other);
@@ -35,6 +51,27 @@ private:
 
    int32_t detID;
    uint16_t tot;
+
+
+   const unsigned ncol = 160;
+   const unsigned nrow = 336;
+   const unsigned nmod = 12;
+   const float shortpix = 250.e-4;
+   const float longpix = 450.e-4;
+   const float rowpitch = 50.e-4;
+   float fX;
+   float fY;
+   float fZ;
+   unsigned fRow;
+   unsigned fCol;
+   unsigned fMod;
+   long fDetectorID;
+   bool hasXYZ;
+   bool hasMCR;
+   bool hasDetectorID;
+   std::array<float, ncol> PixelHit::pixelX;
+   std::array<float, nrow> PixelHit::pixelY;
+   std::array<float, nmod> PixelHit::pixelZ;
 
    ClassDef(ShipPixelHit, 1);
 };
