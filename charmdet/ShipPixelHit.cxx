e8fb7979c5a5df900fba818853d2e8bb7d6ab26f
s6maclim <s6maclim@wn024.baf.physik.uni-bonn.de>
2019-04-01 13:21:59 +0200

    tryin

--- charmdet/ShipPixelHit.cxx
+++ charmdet/ShipPixelHit.cxx
@@ -8,12 +8,12 @@
 
 #include <iostream>
 #include <cmath>
-
+#include <>
 // -----   Standard constructor   ------------------------------------------
 ShipPixelHit::ShipPixelHit(Int_t detID,  Float_t digi)
     : ShipHit(detID, digi) {}
 
-
+/*
 HitID ShipPixelHit::GetPixel()
 {
   int32_t partitionID, frontEndID, row, column;
@@ -42,7 +42,7 @@ int32_t ShipPixelHit::GetModule()
 }
 
 int32_t ShipPixelHit::GetDetectorID(){return fDetectorID; }
-
+*/
 void ShipPixelHit::EndPoints(TVector3 &pixel, int detID, std::unordered_map<int, TVector3> &positionMap) {
 
   int max_detID = 10000000*2 + 1000000*7 + 1000*336 + 80 ;
@@ -58,6 +58,103 @@ void ShipPixelHit::EndPoints(TVector3 &pixel, int detID, std::unordered_map<int,
   pixel.SetY(pixel_pos.Y());
   pixel.SetZ(pixel_pos.Z());
 }
+
+PixelHit::PixelHit(){
+    hasXYZ = false; hasMCR = false; hasDetectorID = false;
+};
+
+PixelHit::~PixelHit(){
+};
+
+void PixelHit::SetXYZ(float x, float y, float z) {
+    fX = x; fX = y; fX = z;
+    hasXYZ = true;
+};
+
+void PixelHit::SetMCR(unsigned mod, unsigned col, unsigned row) {
+    fRow = row; fMod = mod; fCol = col;
+    hasMCR = true;
+};
+
+void PixelHit::SetDetectorID(long detID) {
+    fDetectorID = detID;
+    hasDetectorID = true;
+};
+
+void PixelHit::Convert() {
+    if (!hasXYZ && !hasMCR && !hasDetectorID) exit(0);
+    if (hasMCR) {
+        fX = pixelX[fCol];
+        fY = pixelY[fRow];
+        fZ = pixelY[fMod];
+        hasXYZ = true;
+    }
+
+    if (hasDetectorID) {
+        fCol = fDetectorID%1000 - 1;
+        fRow = (fDetectorID/1000)%1000 - 1;
+        int fe = (fDetectorID/1000000)%10 + 8*(fDetectorID/10000000);
+        fMod = fe/2;
+        if ((fe%2)==1) fCol += 80;
+        hasMCR = true;
+    }
+
+    if (hasMCR) {
+        fX = pixelX[fCol];
+        fY = pixelY[fRow];
+        fZ = pixelY[fMod];
+        hasXYZ = true;
+    }
+}
+
+float PixelHit::GetX() {
+    if (!hasXYZ) Convert();
+    return fX;
+}
+
+float PixelHit::GetY() {
+    if (!hasXYZ) Convert();
+    return fY;
+}
+
+float PixelHit::GetZ() {
+    if (!hasXYZ) Convert();
+    return fZ;
+}
+
+int PixelHit::GetMod() {
+    if (!hasMCR) Convert();
+    return fMod;
+}
+
+int PixelHit::GetRow() {
+    if (!hasMCR) Convert();
+    return fRow;
+}
+
+int PixelHit::GetCol() {
+    if (!hasMCR) Convert();
+    return fCol;
+}
+
+void testit() {
+    std::cout << "pixelX contains:" << std::endl;
+    for (unsigned i=0; i<ncol; i++)
+        std::cout << ' ' << PixelHit::pixelX[i];
+    std::cout << std::endl << std::endl;
+//
+//    std::cout << "pixelY contains:" << std::endl;
+//        for (unsigned i=0; i<nrow; i++)
+//                std::cout << ' ' << PixelHit::pixelY[i];
+//                    std::cout << std::endl << std::endl;
+//                    //
+//                        std::cout << "pixelZ contains:" << std::endl;
+//                            for (unsigned i=0; i<nmod; i++)
+//                                    std::cout << ' ' << PixelHit::pixelZ[i];
+//                                        std::cout << std::endl << std::endl;
+//                                        }
+//
+
 
 // -----   Destructor   ----------------------------------------------------
 // ShipPixelHit::~ShipPixelHit() = default;
